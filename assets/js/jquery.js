949fbe617b5ae6933d94c68b75c9380b5299215f
silvanamarinn <silvana.marinc@gmail.com>
2021-09-10 23:02:32 -0300

    prueba_silvana

---
+++ assets/js/jquery.js
@@ -0,0 +1,10 @@
+var flip = 0;
+/*$( ".despegable" ).click(function() {
+  $( ".despegable_p" ).toggle( flip++ % 2 === 0 );
+});*/
+
+
+
+$('.mostrar').click (function(){
+    $('.ocultar').fadeToggle('slow');
+});
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+  <h4 id="scrollspyHeading1">First heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading2">Second heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading3">Third heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading4">Fourth heading</h4>
+  <p>...</p>
+  <h4 id="scrollspyHeading5">Fifth heading</h4>
+  <p>...</p>
+</div>
